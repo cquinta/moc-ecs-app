2d0a01db5044e64b3e7712a4bb9c3519caad1f8c
Carlos Quintanilha <cquinta@gmail.com>
2025-04-06 13:59:15 -0300

    day3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,21 @@
 module "service" {
-  source           = "/home/cquinta/linuxtips/aws-containers/ecs-service-module"
-  cluster_name     = var.cluster_name
-  service_name     = var.service_name
-  service_port     = var.service_port
-  service_cpu      = var.service_cpu
-  service_memory   = var.service_memory
-  service_listener = data.aws_ssm_parameter.listener.value
+  source                      = "/home/cquinta/linuxtips/aws-containers/ecs-service-module"
+  region                      = var.region
+  cluster_name                = var.cluster_name
+  service_name                = var.service_name
+  service_port                = var.service_port
+  service_cpu                 = var.service_cpu
+  service_memory              = var.service_memory
+  service_listener            = data.aws_ssm_parameter.listener.value
   service_task_execution_role = aws_iam_role.main.arn
+  service_healthcheck         = var.service_healthcheck
+  environment_variables       = var.environment_variables
+  capabilities                = var.capabilities
+  service_launch_type         = var.service_launch_type
+  service_task_count          = var.service_task_count
+  service_hosts               = var.service_hosts
+
+
 
 
 
@@ -18,3 +27,7 @@ module "service" {
 
   ]
 }
+
+task_minimun = var.task_maximum
+task_maximum = var.task_maximum
+scale_type = var.scale_type
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,14 @@ variable "ssm_listener" {}
 variable "ssm_private_subnet_1" {}
 variable "ssm_private_subnet_2" {}
 variable "ssm_private_subnet_3" {}
+variable "environment_variables" {}
+variable "capabilities" {}
+variable "service_healthcheck" {}
+variable "service_launch_type" {}
+variable "service_task_count" {}
+variable "service_hosts" {}
+variable "scale_type" {}
+variable "task_minimun" {}
+variable "task_maximum" {}
+
+
