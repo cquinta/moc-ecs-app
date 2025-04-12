8f43eae438873308ffa59c1f5c6148083de70a42
Carlos Quintanilha <cquinta@gmail.com>
2025-04-12 17:25:19 -0300

    autoscaling cpu

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,36 @@
 module "service" {
-  source                      = "/home/cquinta/linuxtips/aws-containers/ecs-service-module"
-  region                      = var.region
-  cluster_name                = var.cluster_name
-  service_name                = var.service_name
-  service_port                = var.service_port
-  service_cpu                 = var.service_cpu
-  service_memory              = var.service_memory
-  service_listener            = data.aws_ssm_parameter.listener.value
-  service_task_execution_role = aws_iam_role.main.arn
-  service_healthcheck         = var.service_healthcheck
-  environment_variables       = var.environment_variables
-  capabilities                = var.capabilities
-  service_launch_type         = var.service_launch_type
-  service_task_count          = var.service_task_count
-  service_hosts               = var.service_hosts
-
+  source                        = "/home/cquinta/linuxtips/aws-containers/ecs-service-module"
+  region                        = var.region
+  cluster_name                  = var.cluster_name
+  service_name                  = var.service_name
+  service_port                  = var.service_port
+  service_cpu                   = var.service_cpu
+  service_memory                = var.service_memory
+  service_listener              = data.aws_ssm_parameter.listener.value
+  service_task_execution_role   = aws_iam_role.main.arn
+  service_healthcheck           = var.service_healthcheck
+  environment_variables         = var.environment_variables
+  capabilities                  = var.capabilities
+  service_launch_type           = var.service_launch_type
+  service_task_count            = var.service_task_count
+  service_hosts                 = var.service_hosts
+  task_minimun                  = var.task_maximum
+  task_maximum                  = var.task_maximum
+  scale_type                    = var.scale_type
+  scale_out_cpu_threshold       = var.scale_out_cpu_threshold
+  scale_out_ajustment           = var.scale_out_adjustment
+  scale_out_comparison_operator = var.scale_out_comparison_operator
+  scale_out_statistic           = var.scale_out_statistic
+  scale_out_period              = var.scale_out_period
+  scale_out_evaluation_periods  = var.scale_out_evaluation_periods
+  scale_out_cooldown            = var.scale_out_cooldown
+  scale_in_cpu_threshold        = var.scale_in_cpu_threshold
+  scale_in_adjustment           = var.scale_in_adjustment
+  scale_in_comparison_operator  = var.scale_in_comparison_operator
+  scale_in_statistic            = var.scale_in_statistic
+  scale_in_period               = var.scale_in_period
+  scale_in_evaluation_periods   = var.scale_in_evaluation_periods
+  scale_in_cooldown             = var.scale_in_cooldown
 
 
 
@@ -28,6 +44,3 @@ module "service" {
   ]
 }
 
-task_minimun = var.task_maximum
-task_maximum = var.task_maximum
-scale_type = var.scale_type
