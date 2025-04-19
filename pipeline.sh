20975eafb8af9fdc10b23085a22bee08e3048791
Carlos Quintanilha <cquinta@gmail.com>
2025-04-19 11:28:30 -0300

    dev

--- pipeline.sh
+++ pipeline.sh
@@ -49,21 +49,6 @@ curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install
 echo "APP - TEST"
 go test -v ./...
 
-# CI Pipeline for Terraform
-echo "TERRAFORM - CI"
-
-cd ../terraform
-
-# Check Terraform formatting
-echo "TERRAFORM - FORMAT CHECK"
-terraform fmt --recursive --check
-
-# Initialize Terraform with backend config
-#terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
-
-# Validate Terraform configuration
-echo "TERRAFORM - VALIDATE"
-terraform validate
 
 # Build Application
 cd ../app
@@ -113,9 +98,26 @@ docker tag app:latest $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_N
 echo "BUILD - DOCKER PUBLISH"
 docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
 
-# Deploy Infrastructure with Terraform
+
+# CI Pipeline for Terraform
+echo "TERRAFORM - CI"
+
 cd ../terraform
 
+# Check Terraform formatting
+echo "TERRAFORM - FORMAT CHECK"
+terraform fmt --recursive --check
+
+# Initialize Terraform with backend config
+#terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
+
+# Validate Terraform configuration
+echo "TERRAFORM - VALIDATE"
+terraform validate
+
+
+# Deploy Infrastructure with Terraform
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD )
 REPOSITORY_TAG=$AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
 
 # Initialize Terraform again
@@ -129,91 +131,4 @@ terraform plan -var-file=environment/$BRANCH_NAME/terraform.tfvars -var containe
 # Apply Terraform changes
 echo "DEPLOY - TERRAFORM APPLY"
 terraform apply --auto-approve -var-file=environment/$BRANCH_NAME/terraform.tfvars -var container_image=$REPOSITORY_TAG
-
-# Wait for ECS service to stabilize
-echo "DEPLOY - WAIT DEPLOY"
-aws ecs wait services-stable --cluster $CLUSTER_NAME --services $APP_NAME
-# CI DA APP
-
-echo "APP - CI"
-
-cd app/
-
-echo "APP - LINT"
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1
-golangci-lint run ./... -E errcheck
-
-
-echo "APP - TEST"
-go test -v ./...
-
-# CI DO TERRAFORM 
-
-echo "TERRAFORM - CI"
-
-cd ../terraform
-
-echo "TERRAFORM - FORMAT CHECK"
-terraform fmt --recursive --check
-
-terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
-
-echo "TERRAFORM - VALIDATE"
-terraform validate
-
-
-# BUILD APP 
-
-cd ../app
-
-echo "BUILD - BUMP DE VERSAO"
-
-GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
-echo $GIT_COMMIT_HASH
-
-echo "BUILD - LOGIN NO ECR"
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com
-
-
-echo "BUILD - CREATE ECR IF NOT EXISTS"
-
-REPOSITORY_NAME="linuxtips/$APP_NAME"
-
-set +e
-
-# Verificar se o repositório já existe
-REPO_EXISTS=$(aws ecr describe-repositories --repository-names $REPOSITORY_NAME 2>&1)
-
-if [[ $REPO_EXISTS == *"RepositoryNotFoundException"* ]]; then
-  echo "Repositório $REPOSITORY_NAME não encontrado. Criando..."
-  
-  # Criar o repositório
-  aws ecr create-repository --repository-name $REPOSITORY_NAME
-  
-  if [ $? -eq 0 ]; then
-    echo "Repositório $REPOSITORY_NAME criado com sucesso."
-  else
-    echo "Falha ao criar o repositório $REPOSITORY_NAME."
-    exit 1
-  fi
-else
-  echo "Repositório $REPOSITORY_NAME já existe."
-fi
-
-set -e
-
-echo "BUILD - DOCKER BUILD"
-
-docker build -t app . 
-docker tag app:latest $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
-
-
-# PUBLISH APP
-
-echo "BUILD - DOCKER PUBLISH"
-
-docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
-
-
 
